68f355121db09f8e644cc7d1e0493b4bb631b979
Olivier PEREZ <olivier@olivierperez.fr>
2021-03-04 21:07:27 +0100

    title

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/TimerApp.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/TimerApp.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -27,6 +28,10 @@ fun TimerApp(viewModel: TimersViewModel) {
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
+            Text(
+                "Challenge\nTimers",
+                style = MaterialTheme.typography.h1
+            )
             val state: State by viewModel.state.observeAsState(State())
             state.timers.forEach { timer ->
                 TimerView(
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,32 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
-// Set of Material typography styles to start with
+private val headerFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.roboto_bold,
+            weight = FontWeight.W700,
+            style = FontStyle.Normal
+        ),
+    )
+)
+
 val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = headerFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 60.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 20.sp
     )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
